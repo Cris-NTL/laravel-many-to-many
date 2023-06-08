a9dfb141220f6043d0e8c63ac594c7f6d9f020ba
Cris-NTL <natolicristian92@gmail.com>
2023-06-08 18:40:57 +0200

    Add: view of tech into show.blade

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -20,6 +20,7 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::all();
+        $types = Type::all();
         return view('admin.projects.index', compact('projects'));
     }
 
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.admin')
 
 @section('content')
+    <a href="{{ url()->previous() }}">Back</a>
     <h1 class="text-center mt-3">{{ $project->title }}</h1>
+
     @if ($project->type)
         <h6 class="text-end text-muted">{{ $project->type?->name }}</h6>
     @else
         <h6 class="text-end text-muted">Type Unknown</h6>
     @endif
+
+    @if ($project->technologies->isNotEmpty())
+        @foreach ($project->technologies as $technology)
+            <h6 class="text-end text-muted">{{ $technology->name }}</h6>
+        @endforeach
+    @else
+        <h6 class="text-end text-muted">Technology Unknown</h6>
+    @endif
+
     <h6 class="text-end text-muted">{{ $project->slug }}</h6>
     <p class="text-center mt-4">{{ $project->description }}</p>
 @endsection
